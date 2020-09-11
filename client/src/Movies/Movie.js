05099af9e2aa8677c72c0d60c97d4c215353a51b
unknown <nandhinimadan20@gmail.com>
2020-09-10 17:41:21 -0700

    Delete movie completed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams,useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -25,6 +26,16 @@ function Movie({ addToSavedList }) {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+  const deleteMovie =(e)=>{
+    e.preventDefault();
+    axios
+    .delete(`http://localhost:5000/api/movies/${movie.id}`)
+    .then((res)=>{
+      //setting result data to movie
+      setMovie(res.data);
+      push("/")
+    })
+  };
 
   return (
     <div className="save-wrapper">
@@ -33,6 +44,13 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <div className="flex">
+      <div className="update-button" onClick={() => push(`/update-movie/${movie.id}`)}>Update</div> 
+    
+      <div className="update-button" onClick={deleteMovie}> DElete</div>
+      </div>
+     
+     
     </div>
   );
 }
